b27c75eb5093b5f6d776e0a6b33e57a4616fa2ea
DavitHakobyan01 <david.hakobyan1237@gmail.com>
2022-06-14 10:49:58 +0400

    some changes

--- predict.py
+++ predict.py
@@ -6,28 +6,25 @@ import seaborn as sns
 plt.switch_backend('agg')
 
 
-def plot_graphs(X, pred, date):
+def plot_graphs(X, prediction):
     fig = plt.figure(figsize=(8, 5))
-    colors = {0: 'blue', 1: 'red'}
-    # for i in range(len(pred)):
-    #     data = pd.DataFrame(X[i], columns=['Stage', 'Temp'])
-    #     data.sort_values(by=['Stage'], inplace=True)
+    color = 'blue' if prediction == 0 else 'red'
     X.sort_values(by=['Well Temperature Survey Profile Measured Depth'], inplace=True)
-    sns.lineplot(data=X,
-                 x='Well Temperature Survey Profile Measured Depth',
-                 y='Well Temperature Survey Temperature',
-                 marker='o',
-                 color=colors[pred])
+    sns.lineplot(data=X, x='Well Temperature Survey Profile Measured Depth',
+                 y='Well Temperature Survey Temperature', marker='o', color=color)
     plt.grid()
     fig.savefig(r'.\static\images\plot.png')
     plt.close()
 
 
-def predict(filepath, modelpath):
+def predict(filepath, model_path):
     date, df = functions.get_data_for_prediction(filepath)
     X, X_padded = functions.get_sequences(df)
-    model = tensorflow.keras.models.load_model(modelpath)
+
+    model = tensorflow.keras.models.load_model(model_path)
     pred = model.predict(X_padded)
+
     plot_graphs(X, int(pred.round().item()), date)
+
     return date, pred.round(decimals=3).item()
 
